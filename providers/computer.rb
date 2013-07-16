871c1bd6273f490bed49e94e33c18fe64a1ca945
Derek Groh <dgroh@arch.tamu.edu>
2013-07-16 13:20:55 -0500

    computer provider

---
+++ providers/computer.rb
@@ -0,0 +1,40 @@
+require 'mixlib/shellout'
+
+action :add do
+  if exists?
+    Chef::Log.error("The object already exists")
+    new_resource.updated_by_last_action(false)
+  else
+    cmd = "dsadd"
+    cmd << " computer"
+	cmd << " cn=#{new_resource.name},"
+    cmd << "cn=#{new_resource.ou},"
+	cmd << new_resource.domain_name.split(".").map! { |k| "dc=#{k}" }.join(",")
+    
+	new_resource.options.each do |option, value|
+     cmd << " -#{option} #{value}"
+    end 
+  end
+	
+  execute "add_#{new_resource.name}" do
+    command cmd
+  end  
+  new_resource.updated_by_last_action(true)
+end
+
+action :modify do
+
+end
+
+action :move do
+
+end
+
+action :remove do
+
+end
+
+def exists?
+  check = Mixlib::ShellOut.new("dsquery #{new_resource.type} -name #{new_resource.name}").run_command
+  !check.stdout.match("")
+end
