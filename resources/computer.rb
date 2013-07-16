816c46680d7864c78551ccb51abeabecab155597
Derek Groh <dgroh@arch.tamu.edu>
2013-07-16 13:20:07 -0500

    computer resource

---
+++ resources/computer.rb
@@ -0,0 +1,8 @@
+actions :add, :modify, :move, :remove
+default_action :add
+
+attribute :name, :kind_of => String, :name_attribute => true
+attribute :type, :kind_of => String
+attribute :domain_name, :kind_of => String
+attribute :ou, :kind_of => String
+attribute :options, :kind_of => Hash, :default => {}
