443d013470a3eec9b03f5a9824d6bd0d5c79c0eb
Derek Groh <dgroh@arch.tamu.edu>
2014-03-19 16:46:08 -0500

    Update metadata.rb
    mistakes everywhere

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'windows_ad'
 maintainer       'Texas A&M'
 maintainer_email 'dgroh@arch.tamu.edu'
-license          'Apache 2.0'
+license          'MIT'
 description      'Installs/Configures windows active directory'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
