64424b5da43f5227413f59196dfaec0c00efc77c
Derek Groh <dgroh@arch.tamu.edu>
2014-03-19 15:30:55 -0500

    changed to Apache 2.0 License

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,9 @@
 name             'windows_ad'
 maintainer       'Texas A&M'
 maintainer_email 'dgroh@arch.tamu.edu'
-license          'MIT'
+license          'Apache 2.0'
 description      'Installs/Configures windows active directory'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 supports         'windows', ">= 6.2"
-
-depends 'windows'
+depends          'windows'
