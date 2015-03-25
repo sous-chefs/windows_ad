6fd53936380bb8ac2e4c8f8e118ee3fb1e4644db
Derek Groh <derekgroh@gmail.com>
2015-03-25 18:11:56 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'dgroh@arch.tamu.edu'
 license          'MIT'
 description      'Installs/Configures windows active directory'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.2'
+version          '0.4.3'
 supports         'windows', ">= 6.1"
 depends          'windows'
