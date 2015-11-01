5cd7cfdb89f0d39af468f3633b35a900f6984395
derekgroh <derekgroh@gmail.com>
2015-10-31 23:24:17 -0500

    rubocop resources folder

--- resources/user.rb
+++ resources/user.rb
@@ -2,7 +2,7 @@
 # Author:: Derek Groh (<dgroh@arch.tamu.edu>)
 # Cookbook Name:: windows_ad
 # Resource:: user
-# 
+#
 # Copyright 2013, Texas A&M
 #
 # Permission is hereby granted, free of charge, to any person obtaining
@@ -28,11 +28,11 @@
 actions :create, :modify, :move, :delete
 default_action :create
 
-attribute :name, :kind_of => String, :name_attribute => true
-attribute :domain_name, :kind_of => String
-attribute :ou, :kind_of => String
-attribute :options, :kind_of => Hash, :default => {}
-attribute :reverse, :kind_of => String
-attribute :cmd_user, :kind_of => String
-attribute :cmd_pass, :kind_of => String
-attribute :cmd_domain, :kind_of => String
+attribute :name, kind_of: String, name_attribute: true
+attribute :domain_name, kind_of: String
+attribute :ou, kind_of: String
+attribute :options, kind_of: Hash, default: {}
+attribute :reverse, kind_of: String
+attribute :cmd_user, kind_of: String
+attribute :cmd_pass, kind_of: String
+attribute :cmd_domain, kind_of: String
