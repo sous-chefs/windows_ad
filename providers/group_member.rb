dcbc6312d1f64e753d0a515c2b973a3875185f52
derekgroh <derekgroh@gmail.com>
2015-11-02 00:37:13 -0600

    rubocop - providers folder, unable to resolve all offenses

--- providers/group_member.rb
+++ providers/group_member.rb
@@ -26,42 +26,50 @@
 #
 
 action :add do
-  group_dn = CmdHelper.dn(new_resource.group_name, new_resource.group_ou, new_resource.domain_name)
-  user_dn  = CmdHelper.dn(new_resource.user_name,  new_resource.user_ou,  new_resource.domain_name)
+  group_dn = CmdHelper.dn(new_resource.group_name, new_resource.group_ou,
+                          new_resource.domain_name)
+  user_dn  = CmdHelper.dn(new_resource.user_name,  new_resource.user_ou,
+                          new_resource.domain_name)
 
-  if is_member_of?(user_dn, group_dn)
-    Chef::Log.debug("The user is already member of the group")
+  if member_of?(user_dn, group_dn)
+    Chef::Log.debug('The user is already member of the group')
     new_resource.updated_by_last_action(false)
   else
     cmd = dsmod_group_cmd(group_dn, user_dn, '-addmbr')
 
-    Chef::Log.info(print_msg("add #{new_resource.user_name} to #{new_resource.group_name}"))
-    CmdHelper.shell_out(cmd, new_resource.cmd_user, new_resource.cmd_pass, new_resource.cmd_domain)
+    Chef::Log.info(print_msg("add #{new_resource.user_name}
+                             to #{new_resource.group_name}"))
+    CmdHelper.shell_out(cmd, new_resource.cmd_user, new_resource.cmd_pass,
+                        new_resource.cmd_domain)
 
     new_resource.updated_by_last_action(true)
   end
 end
 
 action :remove do
-  group_dn = CmdHelper.dn(new_resource.group_name, new_resource.group_ou, new_resource.domain_name)
-  user_dn  = CmdHelper.dn(new_resource.user_name,  new_resource.user_ou,  new_resource.domain_name)
+  group_dn = CmdHelper.dn(new_resource.group_name, new_resource.group_ou,
+                          new_resource.domain_name)
+  user_dn  = CmdHelper.dn(new_resource.user_name,  new_resource.user_ou,
+                          new_resource.domain_name)
 
-  if is_member_of?(user_dn, group_dn)
+  if member_of?(user_dn, group_dn)
     cmd = dsmod_group_cmd(group_dn, user_dn, '-rmmbr')
 
-    Chef::Log.info(print_msg("remove #{new_resource.user_name} from #{new_resource.group_name}"))
-    CmdHelper.shell_out(cmd, new_resource.cmd_user, new_resource.cmd_pass, new_resource.cmd_domain)
+    Chef::Log.info(print_msg("remove #{new_resource.user_name}
+                             from #{new_resource.group_name}"))
+    CmdHelper.shell_out(cmd, new_resource.cmd_user, new_resource.cmd_pass,
+                        new_resource.cmd_domain)
 
     new_resource.updated_by_last_action(true)
   else
-    Chef::Log.error("User is not a member of the group")
+    Chef::Log.error('User is not a member of the group')
     new_resource.updated_by_last_action(false)
   end
 end
 
 def dsmod_group_cmd(group_dn, user_dn, option)
-  cmd = "dsmod"
-  cmd << " group "
+  cmd = 'dsmod'
+  cmd << ' group '
   cmd << "\""
   cmd << group_dn
   cmd << "\""
@@ -71,9 +79,10 @@ def dsmod_group_cmd(group_dn, user_dn, option)
   cmd << "\""
 end
 
-def is_member_of?(user_dn, group_dn)
+def member_of?(user_dn, group_dn)
   check = CmdHelper.shell_out("dsget group  \"#{group_dn}\"  -members",
-                              new_resource.cmd_user, new_resource.cmd_pass, new_resource.cmd_domain)
+                              new_resource.cmd_user, new_resource.cmd_pass,
+                              new_resource.cmd_domain)
   check.stdout.downcase.include?(user_dn.downcase)
 end
 
--- providers/user.rb
+++ providers/user.rb
@@ -27,11 +27,11 @@
 
 action :create do
   if exists?
-    Chef::Log.debug("The object already exists")
+    Chef::Log.debug('The object already exists')
     new_resource.updated_by_last_action(false)
   else
-    cmd = "dsadd"
-    cmd << " user "
+    cmd = 'dsadd'
+    cmd << ' user '
     cmd << "\""
     cmd << dn
     cmd << "\""
@@ -39,7 +39,8 @@ action :create do
     Chef::Log.info(print_msg("create #{new_resource.name}"))
     cmd << CmdHelper.cmd_options(new_resource.options)
 
-    CmdHelper.shell_out(cmd, new_resource.cmd_user, new_resource.cmd_pass, new_resource.cmd_domain)
+    CmdHelper.shell_out(cmd, new_resource.cmd_user, new_resource.cmd_pass,
+                        new_resource.cmd_domain)
 
     new_resource.updated_by_last_action(true)
   end
@@ -47,60 +48,63 @@ end
 
 action :modify do
   if exists?
-    cmd = "dsmod"
-    cmd << " user "
+    cmd = 'dsmod'
+    cmd << ' user '
     cmd << dn
 
     cmd << CmdHelper.cmd_options(new_resource.options)
 
     Chef::Log.info(print_msg("modify #{new_resource.name}"))
-    CmdHelper.shell_out(cmd, new_resource.cmd_user, new_resource.cmd_pass, new_resource.cmd_domain)
+    CmdHelper.shell_out(cmd, new_resource.cmd_user, new_resource.cmd_pass,
+                        new_resource.cmd_domain)
 
     new_resource.updated_by_last_action(true)
   else
-    Chef::Log.error("The object does not exist")
+    Chef::Log.error('The object does not exist')
     new_resource.updated_by_last_action(false)
   end
 end
 
 action :move do
   if exists?
-    cmd = "dsmove "
+    cmd = 'dsmove '
     cmd << dn
 
     cmd << CmdHelper.cmd_options(new_resource.options)
 
     Chef::Log.info(print_msg("move #{new_resource.name}"))
-    CmdHelper.shell_out(cmd, new_resource.cmd_user, new_resource.cmd_pass, new_resource.cmd_domain)
+    CmdHelper.shell_out(cmd, new_resource.cmd_user, new_resource.cmd_pass,
+                        new_resource.cmd_domain)
 
     new_resource.updated_by_last_action(true)
   else
-    Chef::Log.error("The object does not exist")
+    Chef::Log.error('The object does not exist')
     new_resource.updated_by_last_action(false)
   end
 end
 
 action :delete do
   if exists?
-    cmd = "dsrm "
+    cmd = 'dsrm '
     cmd << dn
-    cmd << " -noprompt"
+    cmd << ' -noprompt'
 
     cmd << CmdHelper.cmd_options(new_resource.options)
 
     Chef::Log.info(print_msg("delete #{new_resource.name}"))
-    CmdHelper.shell_out(cmd, new_resource.cmd_user, new_resource.cmd_pass, new_resource.cmd_domain)
+    CmdHelper.shell_out(cmd, new_resource.cmd_user, new_resource.cmd_pass,
+                        new_resource.cmd_domain)
 
     new_resource.updated_by_last_action(true)
   else
-    Chef::Log.debug("The object has already been removed")
+    Chef::Log.debug('The object has already been removed')
     new_resource.updated_by_last_action(false)
   end
 end
 
 def dn
-  if new_resource.reverse == "true"
-    name = new_resource.name.split(" ").reverse.map! { |k| k }.join("\\, ")
+  if new_resource.reverse == 'true'
+    name = new_resource.name.split(' ').reverse.map! { |k| k }.join('\\, ')
   else
     name = new_resource.name
   end
@@ -111,15 +115,22 @@ def exists?
   cmd_user   = new_resource.cmd_user
   cmd_pass   = new_resource.cmd_pass
   cmd_domain = new_resource.cmd_domain
-  if new_resource.reverse == "true"
-    reverse_name = new_resource.name.split(" ").reverse.map! { |k| k }.join(", ")
-    contact = CmdHelper.shell_out("dsquery contact -name \"#{reverse_name}\"", cmd_user, cmd_pass, cmd_domain)
-    user = CmdHelper.shell_out("dsquery user -name \"#{reverse_name}\"", cmd_user, cmd_pass, cmd_domain)
-    contact.stdout.downcase.include? "dc" or user.stdout.downcase.include? "dc"
+  if new_resource.reverse == 'true'
+    reverse_name = new_resource.name.split(' ').reverse.map! { |k| k }.join(', ')
+    contact = CmdHelper.shell_out("dsquery contact -name \"#{reverse_name}\"",
+                                  cmd_user, cmd_pass, cmd_domain)
+    user = CmdHelper.shell_out("dsquery user -name \"#{reverse_name}\"",
+                               cmd_user, cmd_pass, cmd_domain)
+    contact.stdout.downcase.include?('dc') ||
+      user.stdout.downcase.include?('dc')
   else
-    contact = CmdHelper.shell_out("dsquery contact -name \"#{new_resource.name}\"", cmd_user, cmd_pass, cmd_domain)
-    user = CmdHelper.shell_out("dsquery user -name \"#{new_resource.name}\"", cmd_user, cmd_pass, cmd_domain)
-    contact.stdout.downcase.include? "dc" or user.stdout.downcase.include? "dc"
+    contact = CmdHelper.shell_out("dsquery contact -name
+                                  \"#{new_resource.name}\"", cmd_user,
+                                  cmd_pass, cmd_domain)
+    user = CmdHelper.shell_out("dsquery user -name \"#{new_resource.name}\"",
+                               cmd_user, cmd_pass, cmd_domain)
+    contact.stdout.downcase.include?('dc') ||
+      user.stdout.downcase.include?('dc')
   end
 end
 
