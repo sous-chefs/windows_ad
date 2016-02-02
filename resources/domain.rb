3b3e0d1436440461fbf3ac0391abf66e3452646f
derekam <dgroh@arch.tamu.edu>
2016-02-02 14:28:53 -0600

    rubocop

--- resources/domain.rb
+++ resources/domain.rb
@@ -28,13 +28,13 @@
 actions :create, :delete
 default_action :create
 
-attribute :name, kind_of: 'String', name_attribute: true
-attribute :domain_user, kind_of: 'String', required: true
-attribute :domain_pass, kind_of: 'String', required: true
+attribute :name, kind_of: String, name_attribute: true
+attribute :domain_user, kind_of: String, required: true
+attribute :domain_pass, kind_of: String, required: true
 attribute :restart, kind_of: [TrueClass, FalseClass], required: true, default: true
-attribute :type, kind_of: 'String', default: 'forest'
-attribute :safe_mode_pass, kind_of: 'String', required: true
-attribute :options, kind_of: 'Hash', default: {}
-attribute :local_pass, kind_of: 'String'
-attribute :replica_type, kind_of: 'String', default: 'domain'
-attribute :ou, kind_of: 'String'
+attribute :type, kind_of: String, default: 'forest'
+attribute :safe_mode_pass, kind_of: String, required: true
+attribute :options, kind_of: Hash, :default => {}
+attribute :local_pass, kind_of: String
+attribute :replica_type, kind_of: String, default: 'domain'
+attribute :ou, kind_of: String
