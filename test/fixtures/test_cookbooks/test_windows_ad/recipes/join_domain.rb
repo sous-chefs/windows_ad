28215f0a926a44c921c1a7f1f50c26e21c13d691
unknown <raptor@ITS-SCCM-DELL1.ARCH.TAMU.EDU>
2016-02-09 10:22:30 -0600

    vagrantbox dns

--- test/fixtures/test_cookbooks/test_windows_ad/recipes/join_domain.rb
+++ test/fixtures/test_cookbooks/test_windows_ad/recipes/join_domain.rb
@@ -1,29 +1,21 @@
 user = 'Administrator'
 pass = 'Passw0rd'
 domain = "contoso.local"
-# dc_ip  = node['windows_ad']['dc_ip'] || search(:node, "recipe:test_windows_ad\\:\\:setup_dc")[0]['ipaddress']
 
-# puts "DEBUG:: IP => #{dc_ip}"
-
-WindowsHelper.nic_interface_indexes.each do |index|
-   powershell_script "set DNS servers for NIC #{index}" do
-     code "Set-DNSClientServerAddress -interfaceIndex #{index} -ServerAddresses (\"192.168.1.10\")"
+powershell_script 'Set DNS Server' do
+  case node['os_version']
+  when '6.1'
+    code 'netsh interface ipv4 add dnsserver localhost 192.168.1.13'
+  when '6.2'
+    code 'Set-DnsClientServerAddress -InterfaceAlias \"Local Area Connection 2\" -serveraddress 192.168.1.13'
   end
 end
 
 execute "net user \"#{user}\" \"#{pass}\""
 
-windows_ad_computer 'doesntmatter' do
+windows_ad_computer 'localhost' do
  action :join
  domain_name domain
  domain_user user
  domain_pass pass
 end
-
-# windows_ad_computer 'stilldnm' do
-#  action :unjoin
-#  domain_name domain
-#  domain_user user
-#  domain_pass pass
-#  restart true
-# end
