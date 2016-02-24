290e56c57d783010723a210719fb5d96320f031e
derekam <dgroh@arch.tamu.edu>
2016-02-24 15:36:09 -0600

    add domain_user and domain_pass attributes

--- resources/computer.rb
+++ resources/computer.rb
@@ -30,6 +30,8 @@ default_action :create
 
 attribute :name, kind_of: String, name_attribute: true
 attribute :domain_name, kind_of: String
+attribute :domain_user, kind_of: String
+attribute :domain_pass, kind_of: String
 attribute :ou, kind_of: String
 attribute :options, kind_of: Hash, default: {}
 attribute :cmd_user, kind_of: String
