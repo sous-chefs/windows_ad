1e41734fcb551426cfed986246ad9e59916730d4
derekam <dgroh@arch.tamu.edu>
2016-02-24 15:37:29 -0600

    add DNS server

--- test/fixtures/test_cookbooks/test_windows_ad/recipes/setup_forest.rb
+++ test/fixtures/test_cookbooks/test_windows_ad/recipes/setup_forest.rb
@@ -12,8 +12,13 @@ execute "net user \"#{user}\" \"#{pass}\""
 windows_ad_domain domain do
   type 'forest'
   safe_mode_pass pass
-  netbios_name 'contoso'
   domain_pass pass
   domain_user user
+  case node['os_version']
+  when '6.1'
+    options ({ 'InstallDNS' => 'yes' })
+  when '6.2'
+    options ({ 'InstallDNS' => nil })
+  end
   action :create
 end
