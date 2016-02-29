82cfe718915158410eba9f87d563f27de4a9cc36
G. Arends <gd@wmc.io>
2016-02-29 17:41:44 -0400

    Update test kitchen

--- test/fixtures/test_cookbooks/test_windows_ad/recipes/setup_forest.rb
+++ test/fixtures/test_cookbooks/test_windows_ad/recipes/setup_forest.rb
@@ -3,17 +3,11 @@
 
 include_recipe 'windows_ad::default'
 
-user = 'Administrator'
 pass = 'Password1234###!'
 domain = 'contoso.local'
 
-execute "net user \"#{user}\" \"#{pass}\""
-
 windows_ad_domain domain do
-  type 'forest'
   safe_mode_pass pass
-  domain_pass pass
-  domain_user user
   case node['os_version']
   when '6.1'
     options ({ 'InstallDNS' => 'yes' })
