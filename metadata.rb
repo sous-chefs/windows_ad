2ef73d38160fa105bae90579375d3ef44ed5ed7e
G. Arends <gd@wmc.io>
2016-02-29 17:39:01 -0400

    Update dependencies

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,8 @@ description      'Installs/Configures windows active directory'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.5.0'
 supports         'windows', '>= 6.1'
+chef_version      ">= 12"
+
 depends          'windows'
+depends          'compat_resource', '~> 12.7.1'
 
