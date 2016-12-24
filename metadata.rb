96f27a4bba75e7d0989dd2866b6a5cae82e8084b
Derek Groh <derekgroh@gmail.com>
2016-12-24 09:27:09 -0600

    Foodcritic
    FC064, FC065

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'dgroh@arch.tamu.edu'
 license          'MIT'
 description      'Installs/Configures windows active directory'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+issues_url       'https://github.com/TAMUArch/cookbook.windows_ad/issues'
+source_url       'https://github.com/TAMUArch/cookbook.windows_ad'
 version          '0.5.3'
 supports         'windows', '>= 6.1'
 depends          'windows'
