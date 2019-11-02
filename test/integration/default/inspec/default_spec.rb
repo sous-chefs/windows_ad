e42d24ecdf302612961b9b4e81bddf0cebf3b248
Derek Groh <derekgroh@github.com>
2019-11-02 00:57:35 -0500

    clean up tests; better logic, remove extra code

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -6,55 +6,46 @@
 # The following are only examples, check out https://github.com/chef/inspec/tree/master/docs
 # for everything you can do
 
-# Tests for Windows Server all Families
-if os[:family] == 'windows'
-  describe os[:family] do
-    it { should eq 'windows' }
-  end
-
+# Uses Name Property for Get-WindowsFeature to verify
+if os.release.include?('6.2') || os.release.include?('6.3') || os.release.include?('10')
   describe windows_feature('GPMC') do
     it { should be_installed }
   end
-
+  describe windows_feature('RSAT') do
+    it { should be_installed }
+  end
+  describe windows_feature('RSAT-Role-Tools') do
+    it { should be_installed }
+  end
+  describe windows_feature('RSAT-AD-Tools') do
+    it { should be_installed }
+  end
+  describe windows_feature('RSAT-ADDS-Tools') do
+    it { should be_installed }
+  end
+  describe windows_feature('RSAT-AD-Powershell') do
+    it { should be_installed }
+  end
+  describe windows_feature('RSAT-ADDS-Tools') do
+    it { should be_installed }
+  end
+  describe windows_feature('RSAT-AD-AdminCenter') do
+    it { should be_installed }
+  end
   describe windows_feature('AD-Domain-Services') do
     it { should be_installed }
   end
+end
 
-  # Tests for Windows Server 2008 Family
-  if os[:release] < '6.2'
-    describe os[:release] do
-      it { should be < '6.2' }
-    end
-    describe windows_feature('NetFx3') do
-      it { should be_installed }
-    end
+# Uses execute resource to run dism commands
+if os.release.include?('6.1')
+  describe command('dism /online /get-featureinfo /featurename:NetFx3') do
+    its('stdout') { should include 'State : Enabled' }
   end
-
-  # Tests for Windows Server 2012 and above Family
-  if os[:release] >= '6.2'
-    describe os[:release] do
-      it { should be >= '6.2' }
-    end
-    describe windows_feature('RSAT') do
-      it { should be_installed }
-    end
-    describe windows_feature('RSAT-Role-Tools') do
-      it { should be_installed }
-    end
-    describe windows_feature('RSAT-AD-Tools') do
-      it { should be_installed }
-    end
-    describe windows_feature('RSAT-ADDS-Tools') do
-      it { should be_installed }
-    end
-    describe windows_feature('RSAT-AD-PowerShell') do
-      it { should be_installed }
-    end
-    describe windows_feature('RSAT-ADDS') do
-      it { should be_installed }
-    end
-    describe windows_feature('RSAT-AD-AdminCenter') do
-      it { should be_installed }
-    end
+  describe command('dism /online /get-featureinfo /featurename:Microsoft-Windows-GroupPolicy-ServerAdminTools-Update') do
+    its('stdout') { should include 'State : Enabled' }
+  end
+  describe command('dism /online /get-featureinfo /featurename:DirectoryServices-DomainController') do
+    its('stdout') { should include 'State : Enabled' }
   end
 end
--- test/integration/replica/inspec/replica_spec.rb
+++ test/integration/replica/inspec/replica_spec.rb
@@ -5,7 +5,6 @@
 
 # The following are only examples, check out https://github.com/chef/inspec/tree/master/docs
 # for everything you can do
-describe command("powershell.exe -command \"[adsi]::Exists('LDAP://contoso.local')\"") do
-  its(:exit_status) { should eq 0 }
-  its('stdout') { should match 'True' }
+describe command('[ADSI]::Exists("LDAP://contoso.local")') do
+  its('stdout') { should include 'True' }
 end
