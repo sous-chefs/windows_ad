ee4dd020e48cd87c8b04cb631d33829c5457850c
Derek Groh <derekgroh@github.com>
2019-11-03 20:59:36 -0600

    remove powershell.exe from command

--- test/integration/objects/group_test.rb
+++ test/integration/objects/group_test.rb
@@ -3,10 +3,10 @@
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://www.inspec.io/docs/reference/resources/
 
-describe command('(powershell.exe Get-ADGroup -Identity Group1).name -eq "Group1"') do
+describe command('(Get-ADGroup -Identity Group1).name -eq "Group1"') do
   its('stdout') { should include 'True' }
 end
-  
-describe command('(powershell.exe Get-ADGroup -Identity OU-Group).name -eq "OU-Group"') do
+
+describe command('(Get-ADGroup -Identity OU-Group).name -eq "OU-Group"') do
   its('stdout') { should include 'True' }
 end
