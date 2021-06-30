051362a81f827d709bfd79d29cefe7ba6c059f5f
Julien Lebot <julien.lebot@datadoghq.com>
2021-06-30 11:47:19 +0200

    Add child domain support

--- resources/domain.rb
+++ resources/domain.rb
@@ -12,6 +12,7 @@ default_action :create
 
 property :domain_user, String, required: true
 property :domain_pass, String, required: true
+property :parent_domain_name, String
 property :restart, [TrueClass, FalseClass], required: true
 property :type, String, default: 'forest'
 property :safe_mode_pass, String, required: true
@@ -104,6 +105,10 @@ action_class do
         cmd << 'Install-ADDSForest'
       when 'domain'
         cmd << 'Install-ADDSDomain -Credential $mycreds'
+        if parent_domain_name && !parent_domain_name.empty?
+          cmd << ' -DomainType ChildDomain'
+          cmd << " -ParentDomainName '#{new_resource.parent_domain_name}'"
+        end
       when 'replica'
         cmd << 'Install-ADDSDomainController -Credential $mycreds'
       when 'read-only'
